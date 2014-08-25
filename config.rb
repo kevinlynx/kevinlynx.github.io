e8c232f757914185a4e2646821359b7e6a0b6951
Kevin Lynx <kevinlynx@gmail.com>
2014-08-25 21:45:55 +0800

    upgrade octopress

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/codemacro/"
+http_images_path = "/codemacro/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/codemacro/fonts"
+css_dir = "public/codemacro/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
